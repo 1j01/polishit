7c0a9b3b4f0ae5ba3f47c03cb16361409fd1a5e7
Isaiah Odhner <isaiahodhner@gmail.com>
2025-04-09 12:41:38 -0400

    Adjust metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'PolishIt!',
+  description: 'Turd polishing simulator 💩✨',
   generator: 'v0.dev',
 }
 
