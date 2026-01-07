84b7bb1ec205b5dc8b627d222a2268047917661d
Isaiah Odhner <isaiahodhner@gmail.com>
2026-01-07 16:00:31 -0500

    Tweak default plaque text

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_PLAQUE_TITLE = "Donald J. Trump"
-export const DEFAULT_PLAQUE_SUBTITLE = "He Does His Duty" // "For staggering leadership"
+export const DEFAULT_PLAQUE_SUBTITLE = "He does his duty,\nOnto the American people" // "He Does His Duty" // "For staggering leadership"
